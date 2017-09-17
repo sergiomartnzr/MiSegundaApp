1fb1ef5e84b10fb6d844982d5be58372aaeab33c
Sergio Martinez <sergiomartinez@2806-103e-0006-8f15-1857-71af-88fc-6e2b.ipv6.infinitum.net.mx>
2017-09-16 19:27:31 -0500

    Tarea 2

--- Mi Segunda App/Home.m
+++ Mi Segunda App/Home.m
@@ -8,21 +8,53 @@
 
 #import "Home.h"
 
-@interface Home ()
+@interface Home () {
+
+    int currentValue;
+    int currentColor;
+
+    NSArray *labelTitles;
+    NSArray *labelColors;
+}
 
 @end
 
 @implementation Home
 
+//Initialize variables and return an object of the curent class
+- (instancetype) initWithCoder: (NSCoder*) decoder {
+    if (self = [super initWithCoder:decoder]) {
+        currentValue = 0;
+        currentColor = 0;
+        labelTitles = @[@"Uno", @"Dos", @"Tres", @"Cuatro", @"Cinco"];
+        labelColors = @[[UIColor redColor], [UIColor yellowColor], [UIColor greenColor], [UIColor purpleColor], [UIColor blueColor]];
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 //On press and release action the label text will change to "Mentira!"
 - (IBAction)changeButtonPressed:(id)sender {
-    self.lbWelcome.text = @"Mentira";
+    
+    NSString *currentTitle = labelTitles[currentValue ++];
+    UIColor *currentBackground = labelColors[currentColor ++];
+    
+    self.lbWelcome.text = currentTitle;
+    self.lbWelcome.backgroundColor = currentBackground;
+    
+    if (currentValue == labelTitles.count) {
+        currentValue = 0;
+    }
+    
+    if (currentColor == labelColors.count) {
+        currentColor = 0;
+    }
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
